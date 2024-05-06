cef1870a7c228e69074fd2ab282abdb8166b4147
Masoud Hosseini <MasoudH4190@gmail.com>
2024-05-06 08:53:15 +0330

    define get info user

--- DQL.py
+++ DQL.py
@@ -2,6 +2,16 @@ import mysql.connector
 
 
 
+def get_info_user() :
+    conn=mysql.connector.connect(user='root',password='mh0075239000',host='localhost',database='store')
+    cursor=conn.cursor(dictionary=True)
+    SQL_QURY="""SELECT * FROM user"""
+    cursor.execute(SQL_QURY)
+    result=cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return result
 
 
 
@@ -9,5 +19,6 @@ import mysql.connector
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" :    
     pass
+    
