3602e8ed25002323aa408a7cfe5b32581e1fd40c
Masoud Hosseini <MasoudH4190@gmail.com>
2024-05-13 08:26:21 +0330

    remove persian text from main.py and add all of them in to dict text.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from DDL import *
 from DQL import *
 from DML import *
 from Config import *
+from TEXT import *
+
 
 
 
@@ -94,7 +96,7 @@ def command_start(message):
     if cid in admin :
         user_step.setdefault(cid,2000)
     else:
-        bot.send_message(cid,"به بات خریداز فروشگاه خوش آمدید",reply_to_message_id=message.id)
+        bot.send_message(cid,text['welcome'],reply_to_message_id=message.id)
         user_s=get_user_step(cid)
         if user_s ==1000 :
             if cid in block_user : return
@@ -106,7 +108,7 @@ def command_start(message):
             markup=ReplyKeyboardMarkup(resize_keyboard=True)
             markup.add(button['my_acount'],button['buy'])
             markup.add(button['contact_to_me'],button['help'])
-            bot.send_message(cid,"لطقا از منوی زیر انتخاب کنید",reply_markup=markup)
+            bot.send_message(cid,text['select_menu'],reply_markup=markup)
         
 @bot.message_handler(commands=['main'])
 def main_command(message) :
@@ -116,14 +118,14 @@ def main_command(message) :
         markup=ReplyKeyboardMarkup(resize_keyboard=True)
         markup.add(button['my_acount'],button['buy'])
         markup.add(button['contact_to_me'],button['help'])
-        bot.send_message(cid,"لطقا از منوی زیر انتخاب کنید",reply_markup=markup)
+        bot.send_message(cid,text['select_menu'],reply_markup=markup)
 
 @bot.message_handler(commands=['help'])
 def help_func(message) :
     cid=message.chat.id
     if cid in block_user : return
     if user_step[cid] <2000 :
-        bot.send_message(cid,'راهنمای استفاده از برنامه\n 1- خرید \n بااستفاده از این منو  اول می توان دسته بندی را انتخاب کرد و بعد از ان از لیست کالای مورد نظر را انتخاب و به و سبد خرید اضافه کرد \n 2- حساب کاربری\n شامل دو زیر منو می باشد \n 2.1-سبد خرید\n شامل لسیت اجناس انتخابی که بعد از تایید مراحل واریز وجه و ارسال فاکتور انجام می شود\n 2.2-مشخصات کاربر\n برای تکمیل مشخصات کاربر که شامل :\n نام\n نام خانوادگی\n کد ملی\n شماره موبایل\n آدرس \n 3-راهنمای استفاده از برنامه\n 4- تماس با ما\n می توان به ادمین ها ارتباط برقرار کنید و پیغام دهید')
+        bot.send_message(cid,text['help'])
 
 #text
 
@@ -136,7 +138,7 @@ def button_buy(message) :
     markup.add(button['shirt'],button['tshirt'])
     markup.add(button['home'],button['pants'])
     user_step[cid]=1100
-    bot.send_message(cid,"از این تقسیم بندی یکی را انتخاب کنید",reply_markup=markup)
+    bot.send_message(cid,text['select_breakdown'],reply_markup=markup)
 
 @bot.message_handler(func=lambda message : message.text==button['back'])
 def back_func(message) :
@@ -147,14 +149,14 @@ def back_func(message) :
         markup.add(button['user_profile'],button['cart_basket'])
         markup.add(button['home'])
         user_step[cid]=1200
-        bot.send_message(cid,'یکی از این گزینه هاراانتخاب کنید',reply_markup=markup)
+        bot.send_message(cid,text['select_switch'],reply_markup=markup)
 
 @bot.message_handler(func=lambda message : message.text==button['help'])
 def help_func(message) :
     cid=message.chat.id
     if cid in block_user : return
     if user_step[cid] <2000 :
-        bot.send_message(cid,'راهنمای استفاده از برنامه\n 1- خرید \n بااستفاده از این منو  اول می توان دسته بندی را انتخاب کرد و بعد از ان از لیست کالای مورد نظر را انتخاب و به و سبد خرید اضافه کرد \n 2- حساب کاربری\n شامل دو زیر منو می باشد \n 2.1-سبد خرید\n شامل لسیت اجناس انتخابی که بعد از تایید مراحل واریز وجه و ارسال فاکتور انجام می شود\n 2.2-مشخصات کاربر\n برای تکمیل مشخصات کاربر که شامل :\n نام\n نام خانوادگی\n کد ملی\n شماره موبایل\n آدرس \n 3-راهنمای استفاده از برنامه\n 4- تماس با ما\n می توان به ادمین ها ارتباط برقرار کنید و پیغام دهید')
+        bot.send_message(cid,text['help'])
 
     
 @bot.message_handler(func=lambda message : message.text==button['home'])
@@ -166,7 +168,7 @@ def home_func(message):
         markup=ReplyKeyboardMarkup(resize_keyboard=True)
         markup.add(button['my_acount'],button['buy'])
         markup.add(button['contact_to_me'],button['help'])
-        bot.send_message(cid,"لطقا از منوی زیر انتخاب کنید",reply_markup=markup)
+        bot.send_message(cid,text['select_menu'],reply_markup=markup)
         
        
 @bot.message_handler(func=lambda message : message.text==button['my_acount'])
@@ -178,7 +180,7 @@ def button_buy(message) :
     markup.add(button['user_profile'],button['cart_basket'])
     markup.add(button['home'])
     user_step[cid]=1200
-    bot.send_message(cid,'یکی از این گزینه هاراانتخاب کنید',reply_markup=markup)
+    bot.send_message(cid,text['select_switch'],reply_markup=markup)
 
 @bot.message_handler(func=lambda message : message.text==button['user_profile'])
 def user_Profile_func(message):
@@ -190,7 +192,7 @@ def user_Profile_func(message):
     markup.add(button['back'],button['adress'])
     markup.add(button['home'])
     user_step[cid]=1250
-    bot.send_message(cid,'هرگزینه را انتخاب و مشخصات مربوطه را وارد کنید\n لطفا فارسی وارد کنید',reply_markup=markup)    
+    bot.send_message(cid,text['add_personal'],reply_markup=markup)    
   
 
 @bot.message_handler(func=lambda message : message.text==button['contact_to_me'])
@@ -198,7 +200,7 @@ def contact_to_me_func(message):
     cid=message.chat.id
     if cid in block_user :return
     if user_step[cid] <2000 :
-        bot.send_message(cid,"می توانید به ادمین زیر پیغام بدهید \n @masoud_hosseini216")
+        bot.send_message(cid,text['call_admin'])
 
 
 
