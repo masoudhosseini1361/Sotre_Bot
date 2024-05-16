ce1f5357a8f5cb5a78e5d7e8c55af6e6c5e2c4a1
Masoud Hosseini <MasoudH4190@gmail.com>
2024-05-17 00:41:25 +0330

    define  the function of update user

--- DML.py
+++ DML.py
@@ -13,9 +13,20 @@ def insert_user(cid , fullname= None , mobile_phone = None , national_code=None 
     cursor.close()
     conn.close()
 
+def update_user(cid , fullname , mobile_phone , national_code , adress ):
+    conn=mysql.connector.connect(**db_config)
+    cursor=conn.cursor()
+    SQL_QURY="""UPDATE user SET  fullname = %s, national_code=%s, mobile_phone=%s  , adress=%s 
+                WHERE cid=%s
+    """
+    cursor.execute(SQL_QURY,( fullname ,national_code, mobile_phone  , adress,cid ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 
 if __name__ == "__main__":
     pass
    #insert_user(cid=342165313,step=1000 ,username='masoud216')
+   #update_user(cid=87889742,fullname='masoud hosseini',national_code='0075239000',mobile_phone = '09125227989',adress='prozi street')
