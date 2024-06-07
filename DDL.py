9ae57a13c525079398fb156aa58aab1572c821da
Masoud Hosseini <MasoudH4190@gmail.com>
2024-06-08 01:13:59 +0330

    update the category and kala table

--- DDL.py
+++ DDL.py
@@ -32,14 +32,26 @@ def create_table_category():
     conn=mysql.connector.connect(**db_config)
     cursor=conn.cursor()
     SQL_QURY="""CREATE TABLE IF NOT EXISTS category(
-        name_english   VARCHAR(15) PRIMARY KEY NOT NULL ,
-        name_persian   VARCHAR(15),
+        name_category   VARCHAR(15) PRIMARY KEY NOT NULL ,
         show_category  ENUM('YES','NO') DEFAULT 'YES'
     )    
     """ 
     cursor.execute(SQL_QURY)
     cursor.close()
     conn.close()
+
+# def create_table_category():
+#     conn=mysql.connector.connect(**db_config)
+#     cursor=conn.cursor()
+#     SQL_QURY="""CREATE TABLE IF NOT EXISTS category(
+#         name_english   VARCHAR(15) PRIMARY KEY NOT NULL ,
+#         name_persian   VARCHAR(15),
+#         show_category  ENUM('YES','NO') DEFAULT 'YES'
+#     )    
+#     """ 
+#     cursor.execute(SQL_QURY)
+#     cursor.close()
+#     conn.close()
 
 
 
@@ -60,12 +72,37 @@ def create_table_kala():
         XL             SMALLINT (255) DEFAULT 0,
         XXL            SMALLINT (255) DEFAULT 0,
         PRIMARY KEY(id),
-        FOREIGN KEY (name_category) REFERENCES category (name_english)
+        FOREIGN KEY (name_category) REFERENCES category (name_category)
     )
      """   
     cursor.execute(SQL_QURY)
     cursor.close()
     conn.close()
+
+
+# def create_table_kala():
+#     conn=mysql.connector.connect(**db_config)
+#     cursor=conn.cursor()
+#     SQL_QURY="""CREATE TABLE IF NOT EXISTS kala(
+#         id             INT AUTO_INCREMENT NOT NULL,
+#         kalaname       VARCHAR(50),
+#         buy_price      DOUBLE,
+#         sale_price     INT,
+#         name_category  VARCHAR(15),
+#         kala_date      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+#         image_file_id   VARCHAR(200),
+#         count          SMALLINT (255) DEFAULT 0,
+#         M              SMALLINT (255) DEFAULT 0,
+#         L              SMALLINT (255) DEFAULT 0,
+#         XL             SMALLINT (255) DEFAULT 0,
+#         XXL            SMALLINT (255) DEFAULT 0,
+#         PRIMARY KEY(id),
+#         FOREIGN KEY (name_category) REFERENCES category (name_english)
+#     )
+#      """   
+#     cursor.execute(SQL_QURY)
+#     cursor.close()
+#     conn.close()
     
     
 def create_sale_invoice_table():
@@ -107,9 +144,9 @@ if __name__ == "__main__" :
     pass
 
 
-"""    create_database()
-    create_table_user() 
+    # create_database()
+    # create_table_user() 
     create_table_category()
     create_table_kala()
-    create_sale_invoice_table()
-    create_sale_row_table()"""
+    # create_sale_invoice_table()
+    create_sale_row_table()
